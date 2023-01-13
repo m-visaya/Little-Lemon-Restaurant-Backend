6884502a9cf450ede4bd1c3123ba1e591c45cf70
Marco Visaya <marco.visaya527@gmail.com>
2023-01-14 04:02:13 +0800

    Rename Menu model to Menuitem

--- Restaurant/admin.py
+++ Restaurant/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Menu, Booking
+from .models import MenuItem, Booking
 
-admin.site.register(Menu)
+admin.site.register(MenuItem)
 admin.site.register(Booking)
