ffb544c2ea1a20a1104587dd25f7099aa946bcb7
Marco Visaya <marco.visaya527@gmail.com>
2023-01-14 04:01:54 +0800

    Add menuitem, bookings and auth token urls

--- LittleLemon/urls.py
+++ LittleLemon/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from Restaurant import views
+
+# router = DefaultRouter()
+# router.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('restaurant/', include('Restaurant.urls')),
+    path('api/', include('Restaurant.urls')),
+    path('', include('Restaurant.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
--- Restaurant/urls.py
+++ Restaurant/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='home'),
+    path('bookings', views.BookingViewSet.as_view(), name='booking'),
+    path('api-token-auth/', obtain_auth_token),
+    path('menu-items/', views.MenuItemsView.as_view()),
+    path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
 ]
