078e299e11af91294f608c75ae503d4b590c6a53
Marco Visaya <marco.visaya527@gmail.com>
2023-01-14 04:03:27 +0800

    Create generic views for MenuItems, Bookings and index template

--- Restaurant/views.py
+++ Restaurant/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Booking, MenuItem
+from .serializers import BookingSerializer, MenuSerializer
 
-# Create your views here.
+class MenuItemsView(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class BookingViewSet(generics.ListCreateAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
 def index(request):
     return render(request, 'index.html', {})
+
