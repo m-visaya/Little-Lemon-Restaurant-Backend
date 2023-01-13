6bfd310a9ec7265f3ee652be144ffc464b4c9baf
Marco Visaya <marco.visaya527@gmail.com>
2023-01-14 01:17:21 +0800

    Create Menu and Booking model

--- Restaurant/models.py
+++ Restaurant/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    guests = models.IntegerField()
+    booking_date = models.DateField()
+
+    def __str__(self): 
+        return self.name + " - " + self.booking_date.strftime("%b-%d-%Y")
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    inventory = models.IntegerField()
+
+    def __str__(self):
+      return self.title
